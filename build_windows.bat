1d6952785eecc2ab33a4f7b6fd3d0a8a75797e89
Alic Kutia <tjmacclain98@gmail.com>
2026-09-19 12:28:17 -0600

    Fix Windows Java source list handling

--- build_windows.bat
+++ build_windows.bat
@@ -1,8 +1,10 @@
 @echo off
-setlocal
+setlocal EnableExtensions
 cd /d "%~dp0"
 if not exist build\classes mkdir build\classes
-(for /r src %%F in (*.java) do @echo "%%F") > build\sources.txt
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$ErrorActionPreference='Stop'; Get-ChildItem -Path 'src' -Recurse -Filter '*.java' -File | ForEach-Object { [char]34 + $_.FullName.Replace('\','/') + [char]34 } | Set-Content -Encoding ASCII 'build\sources.txt'"
+if errorlevel 1 exit /b 1
 javac -encoding UTF-8 -d build\classes @build\sources.txt
-if errorlevel 1 exit /b %errorlevel%
+if errorlevel 1 exit /b 1
 echo Java compile passed.
+exit /b 0
